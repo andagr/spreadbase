dd979d037e2c56e571a315296cb9bbf4bd1d1d2c
Andreas Ågren <bandagr@gmail.com>
2026-06-08 23:23:18 +0200

    Added basic window menu

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::menu::{AboutMetadata, MenuBuilder, PredefinedMenuItem, SubmenuBuilder};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +10,41 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            let about_meta = AboutMetadata {
+                name: Some("Spreadbase".to_string()),
+                ..Default::default()
+            };
+            let app_menu = SubmenuBuilder::new(app, "Spreadbase")
+                .item(&PredefinedMenuItem::about(
+                    app,
+                    Some("About Spreadbase"),
+                    Some(about_meta),
+                )?)
+                .separator()
+                .services()
+                .separator()
+                .hide()
+                .hide_others()
+                .show_all()
+                .separator()
+                .quit()
+                .build()?;
+            let file_menu = SubmenuBuilder::new(app, "File")
+                .text("new_file", "New Spreadsheet")
+                .build()?;
+            let edit_menu = SubmenuBuilder::new(app, "Edit")
+                .undo()
+                .redo()
+                .copy()
+                .paste()
+                .build()?;
+            let menu = MenuBuilder::new(app)
+                .items(&[&app_menu, &file_menu, &edit_menu])
+                .build()?;
+            app.set_menu(menu)?;
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
